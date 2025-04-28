289928d3782d8a33a677d154d89d68fa0d2d9b00
diki-haryadi <dk.harryadi@gmail.com>
2025-04-28 13:09:31 +0700

    fix(docker): fixing nginx ssl

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,16 +5,16 @@ mkdir -p /var/www/certbot
 
 # Generate SSL certificate using standalone mode
 certbot certonly --standalone \
-    -d forum-api.haryadi.my.id \
+    -d forumapi.haryadi.my.id \
     --non-interactive \
     --agree-tos \
     -m unpam.dik@gmail.com \
-    --cert-name forum-api.haryadi.my.id
+    --cert-name forumapi.haryadi.my.id
 
 # Copy certificates to nginx ssl directory
 mkdir -p /etc/nginx/ssl
-cp /etc/letsencrypt/live/forum-api.haryadi.my.id/fullchain.pem /etc/nginx/ssl/
-cp /etc/letsencrypt/live/forum-api.haryadi.my.id/privkey.pem /etc/nginx/ssl/
+cp /etc/letsencrypt/live/forumapi.haryadi.my.id/fullchain.pem /etc/nginx/ssl/
+cp /etc/letsencrypt/live/forumapi.haryadi.my.id/privkey.pem /etc/nginx/ssl/
 
 # Start nginx
 nginx -g 'daemon off;'
